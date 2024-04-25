80dc4a1bdcfa8d84352f4c34049e19213ea6935f
SUMAN JANA <sumanjanaled@gmail.com>
2024-04-25 14:08:35 +0530

    auth, file upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import "~/styles/globals.css";
 import Navbar from "./_components/navbar";
 import { ClerkProvider } from '@clerk/nextjs'
+import "@uploadthing/react/styles.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+ 
+import { ourFileRouter } from "~/app/api/uploadthing/core";
 
 
 export const metadata = {
@@ -18,6 +23,15 @@ export default function RootLayout({
     <ClerkProvider>
 
     <html lang="en">
+    <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
 
       <body className=" min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <Navbar />
